55e492deaa44524b770c90f573c47906b41f48cf
Eric Hidalgo <eric.hidalgo@aerolab.co>
2021-02-05 00:15:16 -0300

    chore: installed and configured prettier and lint

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ['./src/pages/*.{js,jsx,ts,tsx}', './src/pages/**/*.{js,jsx,ts,tsx}', './src/components/**/*.{js,jsx,ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {}
   },
   variants: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
-}
+  plugins: []
+};
