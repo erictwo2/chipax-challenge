76cb6dfe82cec90710b3d2ca68c11a7e1bdc446d
Eric Hidalgo <eric.hidalgo@aerolab.co>
2021-02-08 23:27:52 -0300

    refactor: unused conditional statement is removed

--- src/components/home/CharacterCounter/CharacterCounter.tsx
+++ src/components/home/CharacterCounter/CharacterCounter.tsx
@@ -10,10 +10,9 @@ type CharCounter = {
 const CharacterCounter = ({ counters }: { counters: CharCounter[] }): ReactElement => {
   return (
     <div className="flex flex items-start space-x-12">
-      {counters &&
-        counters.map((charCounter, index) => (
-          <CharCounter key={index} total={charCounter.total} text={charCounter.text} suffix={charCounter.suffix} />
-        ))}
+      {counters.map((charCounter, index) => (
+        <CharCounter key={index} total={charCounter.total} text={charCounter.text} suffix={charCounter.suffix} />
+      ))}
     </div>
   );
 };
